cbf1154946115d4e59dfdfaa04c78fd0b8722661
SAary <aarysatardekar2003@gmail.com>
2025-02-11 09:18:09 +0530

    Update firstproject.py
    Added the third variable

--- firstproject.py
+++ firstproject.py
@@ -1,5 +1,6 @@
 #addition of the two numbers 
 no1=int(input("Enter the 1st number :"))
 no2=int(input("Enter the 2nd number :"))
-ansAdd=no1+no2
+no3=int(input("Enter the 3rd number :")
+ansAdd=no1+no2+no2
 print("The addition is ",ansAdd)
