769c0c0de00461048b12db028ac7ac1ed63caca4
SAary <aarysatardekar2003@gmail.com>
2025-02-11 09:45:30 +0530

    Update firstproject.py

--- firstproject.py
+++ firstproject.py
@@ -1,7 +1,8 @@
 #addition of the two numbers 
 no1=int(input("Enter the 1st number :"))
 no2=int(input("Enter the 2nd number :"))
-no3=int(input("Enter the 3rd number :")
-ansAdd=no1+no2+no3
+no3=int(input("Enter the 3rd number :"))
+no4=int(input("Enter the 4th number :"))      
+ansAdd=no1+no2+no3+no4
 print("The addition is ",ansAdd)
-print("The addition is ",ansAdd)
+
